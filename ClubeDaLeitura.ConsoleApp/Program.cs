8377c810134d204a79d13fc254968e2cbcdc5c77
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-04-20 15:08:10 -0300

    Implementado Domínio do módulo Amigo

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -14,6 +14,10 @@ repositorioCaixa.Cadastrar(caixa);
 Revista revista = new Revista("Action Comics", 155, 1990, caixa);
 repositorioRevista.Cadastrar(revista);
 
+Amigo amigo = new Amigo("Joãozinho", "Dona Cleide", "49 ABC222-4353");
+
+amigo.Validar();
+
 while (true)
 {
     Console.Clear();
